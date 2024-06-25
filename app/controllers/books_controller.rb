e27275dcb9771bd02b90626d6434da88647cad24
EC2 Default User <ec2-user@ip-172-31-0-230.ap-northeast-1.compute.internal>
2024-06-25 15:18:48 +0000

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,25 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+     @book = Book.find(params[:id])
   end
 
   def edit
@@ -19,6 +27,7 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.require(:book),permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
+
 end
